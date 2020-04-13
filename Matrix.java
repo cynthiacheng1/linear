ad9a8483f5ca53cbb5c82af9e9678a365b2ce36c
Cynthia <admin@Cynthias-MBP.fios-router.home>
2020-04-13 12:14:59 -0400

    added subtract

--- Matrix.java
+++ Matrix.java
@@ -47,7 +47,7 @@ public class Matrix {
   }
 
   //idea override '+' operation
-  public Matrix addMatrix(Matrix other){
+  public Matrix add(Matrix other){
     if ((this.row == other.row) && (this.col == other.col)){
       Matrix finalMatrix = new Matrix(this.row, this.col);
       for (int i = 0; i < this.row; i++){
@@ -60,29 +60,51 @@ public class Matrix {
     return null;
   }
 
+  public Matrix scalarMult(int num){
+    Matrix resMatrix = new Matrix(this.row, this.col);
+    for (int i = 0; i < this.row; i++){
+      for (int j = 0; j < this.col; j++){
+        resMatrix.matrixArr[i][j] = this.matrixArr[i][j] * num;
+      }
+    }
+    return resMatrix;
+  }
+
+  public Matrix sub(Matrix other){
+    return add(other.scalarMult(-1));
+  }
+
 //matrix function which involves scanner - interaction between user
   public static void main(String[] args) {
+
     Matrix A = new Matrix(3,3);
     A.printMatrix();
+
     int[][] randomInts = {{1,2,3},{1,2,3},{4,5,6}};
     Matrix B = new Matrix(randomInts);
     B.printMatrix();
-    Matrix C = A.addMatrix(B);
+
+    Matrix C = A.add(B);
     C.printMatrix();
+
+    Matrix D = A.sub(B);
+    D.printMatrix();
+
     Scanner in = new Scanner(System.in);
     int userRows, userCols, userData;
     System.out.println("please enter the number of rows for your Matrix");
     userRows = in.nextInt();
     System.out.println("please enter the number of columns");
     userCols = in.nextInt();
-    Matrix D = new Matrix(userRows, userCols);
+    Matrix E = new Matrix(userRows, userCols);
     for (int i = 0; i < userRows; i++){
       for (int j = 0; j < userCols; j++){
-        System.out.println("Enter value for matrix at row: "+i+" and column: "+j+" :");
+        System.out.println("Enter value for matrix at row "+(i+1)+" and column "+(j+1)+" ");
     		userData = in.nextInt();
-    		D.matrixArr[i][j] = userData;
+    		E.matrixArr[i][j] = userData;
       }
     }
-    D.printMatrix();
+    E.printMatrix();
+
   }
 }
